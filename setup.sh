6026f9fe4fe33db9dec2ee165763e12f8825bab0
Michael Bodell <mbodell@gmail.com>
2013-07-20 21:06:50 -0700

    Create setup.sh
    Some commands to run in the environment

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global user.name "Michael Bodell"
+git config --global user.email mbodell@gmail.com
+git config --global core.editor vim
+git config --global merge.tool vimdiff
+
+source .aliases
