529f783c54a5767e1d443934fd8417495f18265b
Michael Bodell <mbodell@gmail.com>
2013-07-21 04:20:15 +0000

    Added git caching

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ git config --global user.name "Michael Bodell"
 git config --global user.email mbodell@gmail.com
 git config --global core.editor vim
 git config --global merge.tool vimdiff
+git config --global credential.helper 'cache --timeout=360000'
 
 source ~/setup-env/.aliases
